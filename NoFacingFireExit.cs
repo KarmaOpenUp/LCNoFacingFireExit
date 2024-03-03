883bbf8dab5701fa3e91ad35da7a26873afa56b6
KarmaOpenUp <43913698+KarmaOpenUp@users.noreply.github.com>
2024-03-03 14:08:02 +0100

    Adding package files

---
+++ NoFacingFireExit.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace NoFacingFireExitBase
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+
+    public class NoFacingFireExitBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Karma.LCNoFacingFireExit";
+        private const string modName = "NoFacingFireExit";
+        private const string modVersion = "1.0.0";
+        private readonly Harmony harmony = new Harmony(modGUID);
+        internal static NoFacingFireExitBase Instance;
+
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            harmony.PatchAll(typeof(NoFacingFireExitBase));
+            harmony.PatchAll(typeof(EntrancePatch));
+        }
+    }
+
+    [HarmonyPatch(typeof(EntranceTeleport))]
+    internal class EntrancePatch
+    {
+        [HarmonyPatch("TeleportPlayer")]
+        [HarmonyPostfix]
+        static void adjustCamera(
+            ref StartOfRound ___playersManager,
+            ref bool ___isEntranceToBuilding,
+            ref int ___entranceId
+            )
+        {
+            PlayerControllerB playerObj = ___playersManager.localPlayerController;
+            if (___isEntranceToBuilding == true && playerObj.isInsideFactory && ___entranceId != 0)
+            {
+                playerObj.thisPlayerBody.Rotate(0f, 180f, 0f, UnityEngine.Space.Self);
+            }
+        }
+    }
+}
